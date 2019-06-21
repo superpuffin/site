0312f721cbc4352d685f28c4b3decbdfb6f57a3c
Yorgos Bos <yorgos.bos@outlook.com>
2019-06-21 17:26:45 +0200

    Add menu animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,29 @@
 (() => {
+    "use strict"
+    var  menu_visible = false;
 document.addEventListener("DOMContentLoaded", ()=>{
     const menu = document.querySelector('.hamburger_menu');
+    const menu_div = document.querySelector('.sidebar_bg');
     const menuNav = document.querySelector('nav');
     const navItems = document.querySelectorAll(".nav-item");
-
-    let menu_visible = false;
+    const social = document.querySelector('.social-icons');
 
     menu.addEventListener('click', ()=>{
         "use strict"
         if(!menu_visible) {
             menu.classList.add('close');
+            menu_div.classList.remove('closed');
             menuNav.classList.add('show');
+            menu_div.classList.add('show');
+            social.classList.add('show');
             navItems.forEach(item => item.classList.add('show'));
             menu_visible = true;
         } else {
             menu.classList.remove('close');
+            menu_div.classList.add('closed');
             menuNav.classList.remove('show');
+            menu_div.classList.remove('show');
+            social.classList.remove('show');
             navItems.forEach(item => item.classList.remove('show'));
             menu_visible = false;
         }
@@ -28,7 +36,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.querySelector(".sidebar_bg").style.top = "0";
-    } else {
+    } else if (menu_visible == false) {
         document.querySelector(".sidebar_bg").style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
